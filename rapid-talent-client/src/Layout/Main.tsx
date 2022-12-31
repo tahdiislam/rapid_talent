212c82d261b65ad5d9b72979d7ac359c6be2a107
Tahdi Islam <tshdi60@gmail.com>
2022-12-31 10:05:53 +0600

    add loging spnner in home page

--- rapid-talent-client/src/Layout/Main.tsx
+++ rapid-talent-client/src/Layout/Main.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CubeSpinner } from "react-spinners-kit";
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footers";
 import Header from "../Shared/Header";
 
 const Main = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div>
-      <Header />
-      <Outlet />
-      <Footer />
+      {loading ? (
+        <div className="w-full h-screen flex justify-center items-center">
+          <CubeSpinner size={40} color="#83C8E4" loading={loading} />
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
--- rapid-talent-client/src/Pages/Home/HomeJobs/HomeJobs.tsx
+++ rapid-talent-client/src/Pages/Home/HomeJobs/HomeJobs.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { FC, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type jobsType = [
   {
