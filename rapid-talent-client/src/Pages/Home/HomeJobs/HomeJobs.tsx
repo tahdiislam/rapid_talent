657df3fe912b678225bfbd378c4c1009b1d60c90
Tahdi Islam <tshdi60@gmail.com>
2022-12-31 17:38:55 +0600

    add fake data

--- rapid-talent-client/src/Pages/Home/HomeJobs/HomeJobs.tsx
+++ rapid-talent-client/src/Pages/Home/HomeJobs/HomeJobs.tsx
@@ -22,10 +22,10 @@ export const HomeJobs = (): JSX.Element | null => {
   // get job data
   useEffect(() => {
     axios
-      .get("http://localhost:5000/jobs/")
+      .get("/jobs.json")
       .then((res) => {
-        setJobs(res.data.result);
-        // console.log(res.data.result[0]);
+        setJobs(res.data);
+        // console.log(res.data);
         setLoading(false);
       })
       .catch((err) => {
@@ -44,7 +44,7 @@ export const HomeJobs = (): JSX.Element | null => {
   return (
     <section id="homeJobs1" className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 -m-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 -m-12">
           {jobs?.map((job: jobType) => (
             <div className="p-12 flex flex-col items-start">
               <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">
@@ -53,9 +53,9 @@ export const HomeJobs = (): JSX.Element | null => {
               <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">
                 {job?.title}
               </h2>
-              <p className="leading-relaxed mb-8">{job?.description}</p>
+              <p className="leading-relaxed mb-8 text-justify">{job?.description.length > 200 ? <>{job?.description.slice(0, 200)}...</> : job?.description}</p>
               <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-                {job?.salary}
+                Salary: {job?.salary}
               </div>
             </div>
           ))}
